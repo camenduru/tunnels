bac8477be440edb369ad0b47febd91019b794c9d
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-26 00:38:30 +0300

    oops

--- scripts/ssh_tunnel.py
+++ scripts/ssh_tunnel.py
@@ -71,8 +71,8 @@ def ssh_tunnel(host: str = LOCALHOST_RUN) -> None:
     os.environ['webui_url'] = tunnel_url
     # os.system("sed -i '/import warnings/a from gradio import strings' /content/stable-diffusion-webui/modules/ui.py")
     # os.system(f'sed -i "/from gradio import strings/a strings.en[\\"PUBLIC_SHARE_TRUE\\"] = f\\"WebUI Colab URL: {tunnel_url}\\"" /content/stable-diffusion-webui/modules/ui.py')
-    strings.en["PUBLIC_SHARE_TRUE"] = f"WebUI Colab URL: {tunnel_url.tunnel}"
-    
+    strings.en["PUBLIC_SHARE_TRUE"] = f"WebUI Colab URL: {tunnel_url}"
+
 
 if cmd_opts.localhostrun:
     print("localhost.run detected, trying to connect...")
