bbc4d2604894260137aee64c966cd1008d8c6070
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-26 00:35:56 +0300

    test

--- scripts/try_cloudflare.py
+++ scripts/try_cloudflare.py
@@ -2,6 +2,8 @@ from pycloudflared import try_cloudflare
 
 from modules.shared import cmd_opts
 
+from gradio import strings
+
 import os
 
 if cmd_opts.cloudflared:
@@ -9,5 +11,6 @@ if cmd_opts.cloudflared:
     port = cmd_opts.port if cmd_opts.port else 7860
     tunnel_url = try_cloudflare(port=port, verbose=False)
     os.environ['webui_url'] = tunnel_url.tunnel
-    os.system("sed -i '/import warnings/a from gradio import strings' /content/stable-diffusion-webui/modules/ui.py")
-    os.system(f'sed -i "/from gradio import strings/a strings.en[\\"PUBLIC_SHARE_TRUE\\"] = f\\"WebUI Colab URL: {tunnel_url.tunnel}\\"" /content/stable-diffusion-webui/modules/ui.py')
+    # os.system("sed -i '/import warnings/a from gradio import strings' /content/stable-diffusion-webui/modules/ui.py")
+    # os.system(f'sed -i "/from gradio import strings/a strings.en[\\"PUBLIC_SHARE_TRUE\\"] = f\\"WebUI Colab URL: {tunnel_url.tunnel}\\"" /content/stable-diffusion-webui/modules/ui.py')
+    strings.en["PUBLIC_SHARE_TRUE"] = f"WebUI Colab URL: {tunnel_url.tunnel}"
