c17af1cffc91b14d969848b29578678a28a24696
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-26 00:23:17 +0300

    oops

--- scripts/try_cloudflare.py
+++ scripts/try_cloudflare.py
@@ -8,6 +8,6 @@ if cmd_opts.cloudflared:
     print("cloudflared detected, trying to connect...")
     port = cmd_opts.port if cmd_opts.port else 7860
     tunnel_url = try_cloudflare(port=port, verbose=False)
-    os.environ['webui_url'] = tunnel_url
+    os.environ['webui_url'] = tunnel_url.tunnel
     os.system("sed -i '/import warnings/a from gradio import strings' /content/stable-diffusion-webui/modules/ui.py")
     os.system('sed -i "/from gradio import strings/a strings.en[\\"PUBLIC_SHARE_TRUE\\"] = f\\"WebUI Colab URL: {tunnel_url}\\"" /content/stable-diffusion-webui/modules/ui.py')
