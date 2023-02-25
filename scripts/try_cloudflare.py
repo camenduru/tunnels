5387718c1e7465ce4e491556637e4750944121cb
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-26 00:19:36 +0300

    oops

--- scripts/try_cloudflare.py
+++ scripts/try_cloudflare.py
@@ -2,6 +2,8 @@ from pycloudflared import try_cloudflare
 
 from modules.shared import cmd_opts
 
+import os
+
 if cmd_opts.cloudflared:
     print("cloudflared detected, trying to connect...")
     port = cmd_opts.port if cmd_opts.port else 7860
