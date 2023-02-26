f29ddc1eb2fe06f9e521c17fcca51d49681d6d8a
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-26 16:08:33 +0300

    oops

--- scripts/ssh_tunnel.py
+++ scripts/ssh_tunnel.py
@@ -70,7 +70,7 @@ def ssh_tunnel(host: str = LOCALHOST_RUN) -> None:
     # print(f" * Running on {tunnel_url}")
     os.environ['webui_url'] = tunnel_url
     colab_url = os.getenv('colab_url')
-    strings.en["PUBLIC_SHARE_TRUE"] = f"Public WebUI Colab URL: {tunnel_url.tunnel}\nPrivate WebUI Colab URL:{colab_url}"
+    strings.en["PUBLIC_SHARE_TRUE"] = f"Public WebUI Colab URL: {tunnel_url}\nPrivate WebUI Colab URL:{colab_url}"
 
 
 def googleusercontent_tunnel():
