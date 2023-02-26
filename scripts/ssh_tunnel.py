94ed182d9ff6d0f64912e1d0aea39c2d252e2fa6
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-27 02:01:45 +0300

    PUBLIC_SHARE_TRUE

--- scripts/ssh_tunnel.py
+++ scripts/ssh_tunnel.py
@@ -70,12 +70,12 @@ def ssh_tunnel(host: str = LOCALHOST_RUN) -> None:
     # print(f" * Running on {tunnel_url}")
     os.environ['webui_url'] = tunnel_url
     colab_url = os.getenv('colab_url')
-    strings.en["SHARE_LINK_MESSAGE"] = f"Public WebUI Colab URL: {tunnel_url}\nPrivate WebUI Colab URL:{colab_url}"
+    strings.en["PUBLIC_SHARE_TRUE"] = f"Public WebUI Colab URL: {tunnel_url}\nPrivate WebUI Colab URL:{colab_url}"
 
 
 def googleusercontent_tunnel():
     colab_url = os.getenv('colab_url')
-    strings.en["SHARE_LINK_MESSAGE"] = f"WebUI Colab URL: {colab_url}"
+    strings.en["PUBLIC_SHARE_TRUE"] = f"WebUI Colab URL: {colab_url}"
 
 
 if cmd_opts.localhostrun:
