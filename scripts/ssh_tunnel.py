8f71949d01189069bfb942eaa3db4a8e7a6635bf
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-27 05:11:37 +0300

    f

--- scripts/ssh_tunnel.py
+++ scripts/ssh_tunnel.py
@@ -70,13 +70,12 @@ def ssh_tunnel(host: str = LOCALHOST_RUN) -> None:
     # print(f" * Running on {tunnel_url}")
     os.environ['webui_url'] = tunnel_url
     colab_url = os.getenv('colab_url')
-    strings.en["SHARE_LINK_MESSAGE"] = f"Public WebUI Colab URL: {tunnel_url}\nPrivate WebUI Colab URL:{colab_url}"
+    strings.en["SHARE_LINK_MESSAGE"] = f"Public WebUI Colab URL: {tunnel_url}"
 
 
 def googleusercontent_tunnel():
     colab_url = os.getenv('colab_url')
     strings.en["SHARE_LINK_MESSAGE"] = f"WebUI Colab URL: {colab_url}"
-
 
 if cmd_opts.localhostrun:
     print("localhost.run detected, trying to connect...")
